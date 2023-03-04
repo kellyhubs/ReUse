1a93b14de0a3553e53d75fa9a22fc0b531ac8a89
kelly <kelly@LAPTOP-V5NKAQK5.localdomain>
2023-03-04 17:45:48 -0600

    btn styling and button compontent

--- home/src/Compontents/button.jsx
+++ home/src/Compontents/button.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-import ',/button.css';
+import './button.css';
 import { Link } from 'react-router-dom';
 
+// custom css variables , can be use var() inside the css page 
+const STYLES = ['btn--primary', 'btn--outline'];
+
+const SIZES = ['btn--medium', 'btn--large'];
 
 // pass in properties 
 export const Button = (
@@ -9,8 +13,20 @@ export const Button = (
     type,
     onClick,
     buttonStyle, 
-    ButtonSize
+    buttonSize
 }) => {
-    // if the component have the button style make it buttonstyle that is created (true), else set the value to the styles of the first index/option in the array (false)
-    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0]
+    // if the component have the buttonStyle (true) use the buttonStyle property, else set the value to the styles of the first index/option in the array (false)
+    // btn-primary is default style for the button if  buttonStyles is not applied 
+    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
+
+    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0] // same concept as above 
+
+        return (
+            <Link to='/signup' className='btn-small'>
+                <button className={`btn ${checkButtonStyle} ${checkButtonSize}`} onClick={onClick} type={type}>
+                    {children} 
+                    {/* whatever value is put inside the button, it's rendered by the children property */}
+                </button>
+            </Link>
+        )
 };
