3eab22e6822c26fdf2134dd1ae2d9d8418affa90
kelly <kelly@LAPTOP-V5NKAQK5.localdomain>
2023-03-05 09:11:05 -0600

    added styling to the homepage

--- home/src/Compontents/FirstSection/FirstElements.jsx
+++ home/src/Compontents/FirstSection/FirstElements.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { MdKeyboardArrowRight, MdArrowForward } from 'react-icons/md' // importing the icons from react icon package
 
 // styling the first section
 export const FirstContainer = styled.div`
@@ -31,4 +32,66 @@ export const VideoBg = styled.video`
     -o-object-fit: cover;
     object-fit: cover;
     background: #232134;
+`;
+
+// styling the text on the video 
+export const FirstContent = styled.div`
+    z-index: 3;
+    max-width: 1200px;
+    position: absolute;
+    padding: 8px 12px; 
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+// styling the H1 text
+export const FirstH1 = styled.h1`
+    color: #fff;
+    font-size: 48px;
+    text-align:center;
+
+    @media screen and (max-width: 768px){
+        font-size: 40px;
+    }
+
+    @media screen and (max-width: 480px){
+        font-size: 32px;
+    }
+`;
+
+// styling the p text
+export const FirstP = styled.p`
+    margin-top:24px;
+    color: #fff;
+    font-size: 24px;
+    text-align: center;
+    max-width: 600px;
+
+    @media screen and (max-width: 768px){
+        font-size: 24px;
+    }
+
+    @media screen and (max-width: 480px){
+        font-size: 18px;
+    }
+`;
+
+// styling the  button 
+export const FirstBtnWrapper = styled.div`
+    margin-top: 32px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+// styling the button arrow icon
+export const ArrowForward = styled(MdArrowForward)`
+    margin-left: 8px;
+    font-size: 20px;
+`;
+
+export const ArrowRight = styled(MdKeyboardArrowRight)`
+    margin-left: 8px;
+    font-size: 20px;
 `;
--- home/src/Compontents/FirstSection/index.jsx
+++ home/src/Compontents/FirstSection/index.jsx
@@ -1,14 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Video from '../../videos/video.mp4'
-import { FirstContainer, FirstBg, VideoBg} from './FirstElements'
+import { FirstContainer, FirstBg, VideoBg, FirstContent, FirstH1, FirstP, FirstBtnWrapper, ArrowForward, ArrowRight } from './FirstElements'
 
 const FirstSection = () => {
+
+const [hover, setHover] = useState(false);
+// updating the state 
+
+const onHover = () => {
+    setHover(!hover)
+}
+// when the mouse hovers, the state of the page changes (true) and changes the button
+
+
+
 return (
     <FirstContainer id='home'>
         <FirstBg>
             {/* inserting a video that loops in the background */}
             <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
         </FirstBg>
+
+
+{/* text on the video */}
+        <FirstContent>
+        <FirstH1>Building a Sustainable Future with ReUse</FirstH1>
+        <FirstP>
+        Sign up for a new account today and make the change for a better lifestyle! 
+        </FirstP>
+
+        {/* button */}
+        <FirstBtnWrapper>
+        <Button to='signup' onMouseEnter={onHover} onMouseLeave={onHover}>
+            {/* Changes the icon of the arrow in the button (animation), when the mouse hovers the button the arrow icon toggles */}
+            Get Started{hover ? <ArrowForward /> : <ArrowRight /> }
+        </Button>
+        </FirstBtnWrapper>
+
+        </FirstContent>
     </FirstContainer>
 );
 }
