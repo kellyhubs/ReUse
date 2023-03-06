f4eab303a72515b2b18a6fde109f8dcc223e7352
kelly <kelly@LAPTOP-V5NKAQK5.localdomain>
2023-03-06 02:19:38 -0600

    fix route between pages

--- home/src/App.jsx
+++ home/src/App.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import './App.css'
-import {BrowserRouter as Router} from 'react-router-dom' //react router to move between pages
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom' //react router to move between pages
 import Home from './pages'
+import SigninPage from './pages/signin';
 
 
 
 function App() {
   return(
     <Router>
-      {/* homepage */}
-      <Home />
+      {/* Routes component is used to render components only when the path will be matched. Otherwise, it returns to the not found component. */}
+      <Routes>
+        {/* route to homepage */}
+        <Route path="/" element={<Home />}  />
+
+        {/* route to sign in page */}
+        <Route path="/signin" element={<SigninPage />} />
+      </Routes>
     </Router>
   );
 }
---
+++ home/src/pages/signin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const SigninPage = () => {
+return (
+    <div>
+        <h1>Sign in page (add code here I already added route between pages [no back end yet]) </h1>
+    </div>
+)
+}
+
+export default SigninPage;
