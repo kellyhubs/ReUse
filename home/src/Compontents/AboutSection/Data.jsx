d1e93edd3547e8bef925d5e2c6dc66166c6007e6
kelly <kelly@LAPTOP-V5NKAQK5.localdomain>
2023-03-05 19:14:57 -0600

    added imgs

--- home/src/Compontents/AboutSection/Data.jsx
+++ home/src/Compontents/AboutSection/Data.jsx
@@ -1,2 +1,21 @@
 // This data file is where we can pass in different values and can update the design the application without hardcoding properties 
     // and is used to  create reusable compontents 
+
+
+// about section object
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'The Goals',
+    headline: 'Encouraging a Sustainable Lifestyle',
+    description: 'Steer people away from mindless spending habits of short term happiness that heavily contribute to environmental distress, and create mutual identity within a community surrounding sustainability.',
+    buttonLabel: 'Get Started',
+    imgStart: false,
+    img:require('../../images/svg-1.svg'),
+    alt: 'Community',
+    dark: true,
+    primary: true,
+    darkText: false
+}
