69b877099b54acce88f991c7a3215ac81d09057b
kelly <kelly@LAPTOP-V5NKAQK5.localdomain>
2023-03-11 10:17:39 -0600

    updated user

--- Routes/users.js
+++ Routes/users.js
@@ -1,8 +1,34 @@
 // ROUTES FOR USERS 
+const User = require("../models/User");
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
 
+// UPDATING USER PROFILE
+router.put("/:id", async (req,res) => { 
+    //verifying user id -> if they match the user can update their account , not match -> cannot update/delete, unless admin
+    if(req.body.userId === req.params.id || req.body.isAdmin){
+        //user wants to update password 
+        if(req.body.password){
+            try{
+                const salt = await bcrypt.genSalt(10); //generating a new password 
+                req.body.password = await bcrypt.hash(req.body.password, salt); //hashing it & update it
+            } catch(err){
+                return res.status(500).json(err);
+            }
+        }
+// updating user info
+        try{
+            const user = await User.findByIdAndUpdate(req.params.id, {$set: req.body}); //automatically update/sets all inputs in the req.body
+            res.status(200).json("Account has been updated"); //sends a message that updated completed
+        } catch(err) {
+            return res.status(500).json(err); 
+        }
 
-// updating the user 
+    } else {
+        return res.status(403).json("You cannot update this account")
+    }
+})
+
 // deleting the user
 // getting the user 
 // follow users 
