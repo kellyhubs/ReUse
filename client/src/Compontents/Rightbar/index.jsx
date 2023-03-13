52a2a43e5ed0f70d863709be9ec18ed82a95a1c8
kelly <kelly@LAPTOP-V5NKAQK5.localdomain>
2023-03-13 02:45:06 -0500

    cry

--- client/src/Compontents/Rightbar/index.jsx
+++ client/src/Compontents/Rightbar/index.jsx
@@ -4,7 +4,14 @@ import "./Rightbar.css"
 export function Rightbar() {
   return (
     <div className='rightbar'>
-        rightbar
+      <div className="rightbarWrapper">
+        <div className="lovingContainer">
+          <img className="lovingImg" src="/assets/loving.png" alt="love" />
+          <span className="lovingText"><b>Tony Tony Chopper</b> and <b>2 other friends</b> have a community event today!</span>
+        </div>
+        {/* fake ad lol  */}
+        <img className='rightbarAd' src="/assets/ad.png" alt="ad" />
+      </div>
     </div>
   )
 }
