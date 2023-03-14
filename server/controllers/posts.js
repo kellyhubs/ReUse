edb6ff604c35e524cf3137e4a8197bcf235c2487
kelly <kellypenn98@gmail.com>
2023-03-14 00:16:05 -0500

    create and get route backend

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,6 +1,29 @@
 // creating all of the handlers for our routes 
 // putting all the logic and requests in here and then import to the routes 
 
-export const getPosts = (req,res) =>{
-    res.send('THIS WORKS!');
+import PostMessage from '../models/postMessage.js';
+
+
+// GET ROUTE
+export const getPosts = async (req,res) =>{
+    try{
+        const postMessages = await PostMessage.find(); //finding something in a model takes time so it's a async function to get the data 
+        console.log(postMessages);
+        res.status(200).json(postMessages);
+    } catch(err){
+        res.status(400).json({ message: err.message }); //send/catches error if cannot find data
+    }
+}
+
+// CREATE ROUTE
+export const createPost = async (req,res) =>{
+    const posts = req.body // request body (from the form)
+    const newPost = new PostMessage(posts); //creating new data and sending it through as a new post
+
+    try{
+        await newPost.save() //saving is a async function
+        res.status(201).json(newPost); //successful for post creation
+    } catch(err){
+        res.status(409).json({ messsage: err.message }); //else catch error 
+    }
 }
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,13 @@
 // CRUD ROUTES => localhost: 3001/posts
 import express from 'express';
-import { getPosts } from '../controllers/posts.js' //grabbing from the controller folder
+import { getPosts, createPost } from '../controllers/posts.js' //grabbing from the controller folder
 
 const router = express.Router(); //router
 
-// localhost: 3001/posts
+// localhost: 3001/posts -> GET REQUEST
 router.get('/', getPosts);
+
+// CREATE REQUEST
+router.post('/', createPost);
 
 export default router; //exporting the router
